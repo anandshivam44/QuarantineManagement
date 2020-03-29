18feb12247e3b4cc554fc1f7a06a1173a0aca0b2
CoderAbt888 <abhaytr81359@gmail.com>
2020-03-29 09:02:15 +0530

    Ok Telephone Directory

--- app/src/main/java/com/example/quarantinemanagement/Telephone.java
+++ app/src/main/java/com/example/quarantinemanagement/Telephone.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 
 public class Telephone extends AppCompatActivity {
     //////////////////////////////////
+    //abhay
 
     private RecyclerView mRecyclerview;
     private RecyclerView.Adapter madapter;
