9e42daea273cfe7fb2b80fd94bb1e7a739d47788
Shivam Anand <anand.shivam44@yahoo.com>
2020-03-29 19:03:18 +0530

    web view

--- app/src/main/java/com/example/quarantinemanagement/MainActivity.java
+++ app/src/main/java/com/example/quarantinemanagement/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.quarantinemanagement;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -12,21 +13,34 @@ import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import java.lang.SecurityException;
+import java.security.Permission;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 
 public class MainActivity extends AppCompatActivity {
     private int PERMISSION_CODE = 1;
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
     private ImageView SMS, Battery, GPS, autoStart, btn5, btn6;
     PowerManager pm;
     Button nextButton;
     String[] perms = {"android.permission.FINE_LOCATION","android.permission.COARSE_LOCATION"};
     int permsRequestCode = 200;
+    private Context context;
+    private Activity activity;
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private View view;
 
 
     @RequiresApi(api = Build.VERSION_CODES.P)
@@ -34,7 +48,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        context = getApplicationContext();
+        activity = this;
 
         SMS = findViewById(R.id.give_permission_sms);
         Battery = findViewById(R.id.give_permission_battery);
@@ -65,114 +80,124 @@ public class MainActivity extends AppCompatActivity {
         GPS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                requestPermissions(perms, permsRequestCode);
 
+                boolean b= checkPermissionGPS();
+                if (b==false){
+                    requestPermissionGPS();
+                }
 
+            }
+        });
+        SMS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean b=checkPermissionSendSMS();
+                if (b){
+                    requestPermissionSendSMS();
+                }
+
+                boolean c= checkPermissionReadSMS();
+                if (c==false){
+                    requestPermissionReceiveSMS();
+                }
 
             }
         });
 
     }
 
-//        btn1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //btn1.setBackgroundColor(Color.parseColor("#27ae60"));
-//                btn1.setBackground(ContextCompat.getDrawable(MainActivity.this,R.drawable.btn_back3));
-//
-//
-//                    if(ContextCompat.checkSelfPermission(MainActivity.this,
-//                            Manifest.permission.INTERNET)== PackageManager.PERMISSION_GRANTED)
-//                    {
-//                        Toast.makeText(MainActivity.this,"permission already granted",Toast.LENGTH_SHORT).show();
-//                    }
-//                    else
-//                    {
-//                        requestPermission();
-//                    }
-//
-//            }
-//        });
-//
-//        btn2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                btn2.setBackground(ContextCompat.getDrawable(MainActivity.this,R.drawable.btn_back3));
-//                startActivity(new Intent(MainActivity.this,DrawerActivity.class));
-//            }
-//        });
-//        btn3.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                btn3.setBackground(ContextCompat.getDrawable(MainActivity.this,R.drawable.btn_back3));
-//                startActivity(new Intent(MainActivity.this,RegistrationActivity.class));
-//            }
-//        });
-//        btn4.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                btn4.setBackground(ContextCompat.getDrawable(MainActivity.this,R.drawable.btn_back3));
-//                startActivity(new Intent(MainActivity.this,Telephone.class));
-//
-//
-//            }
-//        });
-//        btn5.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                btn5.setBackground(ContextCompat.getDrawable(MainActivity.this,R.drawable.btn_back3));
-//            }
-//        });
-//
-//        btn6.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                btn6.setBackground(ContextCompat.getDrawable(MainActivity.this,R.drawable.btn_back3));
-//            }
-//        });
+    private boolean checkPermissionSendSMS() {
+        int result = ContextCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS);
+        if (result == PackageManager.PERMISSION_GRANTED){
 
-
-    private void requestPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)) {
-            new AlertDialog.Builder(this)
-                    .setTitle("permission needed")
-                    .setMessage("kindly grant permission")
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions(MainActivity.this,
-                                    new String[]{Manifest.permission.INTERNET}, PERMISSION_CODE);
-                        }
-                    })
-                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    })
-                    .create().show();
-
+            return true;
 
         } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, PERMISSION_CODE);
-        }
 
+            return false;
+
+        }
+    }
+
+    private boolean checkPermissionGPS(){
+        int result = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (result == PackageManager.PERMISSION_GRANTED){
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+    }
+    private boolean checkPermissionReadSMS(){
+        int result = ContextCompat.checkSelfPermission(context, Manifest.permission.READ_SMS);
+        if (result == PackageManager.PERMISSION_GRANTED){
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+    }
+
+
+
+    private void requestPermissionGPS(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity,Manifest.permission.ACCESS_FINE_LOCATION)){
+
+            Toast.makeText(context,"GPS permission allows us to access location data. Please allow in App Settings for additional functionality.",Toast.LENGTH_LONG).show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(activity,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},PERMISSION_REQUEST_CODE);
+        }
+    }
+    private void requestPermissionReceiveSMS(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity,Manifest.permission.READ_SMS)){
+
+            Toast.makeText(context,"GPS permission allows us to access location data. Please allow in App Settings for additional functionality.",Toast.LENGTH_LONG).show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(activity,new String[]{Manifest.permission.READ_SMS},PERMISSION_REQUEST_CODE);
+        }
+    }
+    private void requestPermissionSendSMS(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity,Manifest.permission.SEND_SMS)){
+
+            Toast.makeText(context,"GPS permission allows us to access location data. Please allow in App Settings for additional functionality.",Toast.LENGTH_LONG).show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(activity,new String[]{Manifest.permission.SEND_SMS},PERMISSION_REQUEST_CODE);
+        }
     }
 
 
     @Override
-    public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults){
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-        switch(permsRequestCode){
+                   // Snackbar.make(view,"Permission Granted, Now you can access location data.",Snackbar.LENGTH_LONG).show();
 
-            case 200:
+                } else {
 
-                boolean locationAccepted = grantResults[0]==PackageManager.PERMISSION_GRANTED;
-                boolean cameraAccepted = grantResults[1]==PackageManager.PERMISSION_GRANTED;
+                   // Snackbar.make(view,"Permission Denied, You cannot access location data.",Snackbar.LENGTH_LONG).show();
 
+                }
                 break;
-
         }
-
     }
+
+
+
 }
